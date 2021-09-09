85ab3efd781c2eaa6ac211c856ac99d306a8b7da
Nguyen Vu Hao <haonv@paxcreation.com>
2021-09-09 16:11:22 +0700

    add started code

---
+++ loginbasic/lib/core/services/api.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:loginbasic/core/models/post.dart';
+import 'package:loginbasic/core/models/user.dart';
+import 'package:loginbasic/core/models/comment.dart';
+
+/// The service responsible for networking requests
+class Api {
+  static const endpoint = 'https://jsonplaceholder.typicode.com';
+
+  var client = new http.Client();
+
+  Future<User> getUserProfile(int userId) async {
+    // Get user profile for id
+    var response = await client.get('$endpoint/users/$userId');
+
+    // Convert and return
+    return User.fromJson(json.decode(response.body));
+  }
+
+  Future<List<Post>> getPostsForUser(int userId) async {
+    List<Post> posts = [];
+    // Get user posts for id
+    var response = await client.get('$endpoint/posts?userId=$userId');
+
+    // parse into List
+    var parsed = json.decode(response.body) as List<dynamic>;
+
+    // loop and convert each item to Post
+    for (var post in parsed) {
+      posts.add(Post.fromJson(post));
+    }
+
+    return posts;
+  }
+
+  Future<List<Comment>> getCommentsForPost(int postId) async {
+    List<Comment> comments = [];
+
+    // Get comments for post
+    var response = await client.get('$endpoint/comments?postId=$postId');
+
+    // Parse into List
+    var parsed = json.decode(response.body) as List<dynamic>;
+
+    // Loop and convert each item to a Comment
+    for (var comment in parsed) {
+      comments.add(Comment.fromJson(comment));
+    }
+
+    return comments;
+  }
+}
